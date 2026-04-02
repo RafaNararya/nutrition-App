3d96cbe91f7578252a6ca3e52d3bc2b21d2c1726
RafaNararya <mrafanararya@gmail.com>
2026-04-01 23:32:51 -0500

    Initial Seeding Process

---
+++ app/seeding.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+food = pd.read_csv("csvFiles/food.csv")
+foodNutrient = pd.read_csv("csvFiles/food_nutrient.csv")
+nutrient = pd.read_csv("csvFiles/nutrient.csv")
+
+targetNutrients = [1008, 1003, 1004, 1005]
+filteredNutrients = foodNutrient[foodNutrient['nutrient_id'].isin(targetNutrients)]
+
+pivotNutrients = filteredNutrients.pivot(index = "fdc_id", columns = "nutrient_id", values = "amount")
+pivotNutrients.columns = ["Protein", "Fats", "Carbs", "Calories"]
+pivotNutrients = pivotNutrients.reset_index()
+print(pivotNutrients)
